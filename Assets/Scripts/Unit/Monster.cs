8201e0c8c0450b7d10f174344b92841e34962cfe
wuyuhui_21 <wuyuhui_21@qq.com>
2022-11-22 09:49:38 +0800

    Hello 张三

--- Assets/Scripts/Unit/Monster.cs
+++ Assets/Scripts/Unit/Monster.cs
@@ -7,7 +7,7 @@ public class Monster : Unit
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello main!");
+        Debug.Log("Hello уехЩ!");
     }
 
     // Update is called once per frame
